305b1543f3fb40ffdc884414a85b1d9e08842a9e
José Murillo <jooseemurillo@gmail.com>
2020-11-16 08:44:11 -0300

    🔥 Component Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { GoThreeBars } from 'react-icons/go';
+import { SiYoutube } from 'react-icons/si';
+import { ImSearch } from 'react-icons/im';
+import { MdNotifications } from 'react-icons/md';
+
+import { Container, YoutubeLogo } from './styles';
+
+const Header = () => {
+  return (
+    <Container>
+      <div>
+        <GoThreeBars color="#fff" size={25} />
+        <YoutubeLogo>
+          <SiYoutube color="#fe0000" size={30} />
+          <span>Youtube</span>   
+        </YoutubeLogo>
+      </div>
+
+      <form>
+        <input type="text"/>
+        <button>
+          <ImSearch size={15} color="#585858" />
+        </button>
+      </form>
+
+    <div>
+      <MdNotifications size={25} color="#fff" />
+      <MdNotifications size={25} color="#fff" />
+      <MdNotifications size={25} color="#fff" />
+      <img src="https://avatars0.githubusercontent.com/u/43470555?s=460&u=d5a88d8aa43e4fc0f53a417320d1cc6fb4fc13d5&v=4" alt="Profile"/>
+    </div>
+  </Container>
+  );
+};
+
+export default Header;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,15 +2,10 @@ import React from 'react';
 
 import { 
   Container,
-  Header,
-  YoutubeLogo,
   Main
 } from './styles';
 
-import { GoThreeBars } from 'react-icons/go';
-import { SiYoutube } from 'react-icons/si';
-import { ImSearch } from 'react-icons/im';
-import { MdNotifications } from 'react-icons/md';
+import Header from '../../components/Header';
 
 import SideBar from '../../components/SideBar';
 import CardVideo from '../../components/CardVideo';
@@ -18,29 +13,7 @@ import CardVideo from '../../components/CardVideo';
 const Home = () => {
   return (
     <Container>
-      <Header>
-        <div>
-          <GoThreeBars color="#fff" size={25} />
-          <YoutubeLogo>
-            <SiYoutube color="#fe0000" size={30} />
-            <span>Youtube</span>   
-          </YoutubeLogo>
-        </div>
-
-        <form>
-          <input type="text"/>
-          <button>
-            <ImSearch size={15} color="#585858" />
-          </button>
-        </form>
-
-        <div>
-          <MdNotifications size={25} color="#fff" />
-          <MdNotifications size={25} color="#fff" />
-          <MdNotifications size={25} color="#fff" />
-          <img src="https://avatars0.githubusercontent.com/u/43470555?s=460&u=d5a88d8aa43e4fc0f53a417320d1cc6fb4fc13d5&v=4" alt="Profile"/>
-        </div>
-      </Header>
+      <Header />
       <Main>
         <SideBar />
         <CardVideo />
